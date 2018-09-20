add54080dd22833de2e2d6b62d11eefe97479dcf
jinhee kim <mail@kimjinhee.me>
2018-09-20 19:01:35 +0900

    class 사용해 보기

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-items',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
 
-  item = 'first item';
+  item: Item = {
+    id: 1,
+    text: 'my first todo is studying Angular'
+  };
+
   constructor() { }
 
   ngOnInit() {
